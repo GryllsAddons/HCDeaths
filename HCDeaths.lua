dc988a9310da8a4ab37cb3e80fe409b799cfe56b
GryllsAddons <107083057+GryllsAddons@users.noreply.github.com>
2023-04-03 20:25:19 +0100

    Code cleanup

--- HCDeaths.lua
+++ HCDeaths.lua
@@ -20,7 +20,6 @@ local killerName
 local killerLevel
 local killerClass
 local environment
-
 local logged
 
 function HCDeath.resetVariables()
@@ -34,7 +33,8 @@ function HCDeath.resetVariables()
 	killerName = nil
 	killerLevel = nil
 	killerClass = nil
-	environment = nil	
+	environment = nil
+	logged = nil			
 end
 
 function HCDeath.tablelength()
@@ -46,12 +46,8 @@ function HCDeath.tablelength()
 end
 
 function HCDeath.logDeath()
-	if (environment) then killerName = "Natural Causes" end
-	if ((not environment) and (deathType == "PVE")) then killerClass = "NPC" end
 	table.insert(HCDeaths, sdate .. "," .. stime .. "," .. tostring(deathType) .. "," .. tostring(zone) .. ","  .. tostring(playerName) .. "," .. tostring(playerLevel) .. "," .. tostring(playerClass) .. "," .. tostring(killerName) .. "," .. tostring(killerLevel) .. "," .. tostring(killerClass))
-	local info = ChatTypeInfo["SYSTEM"]
-	DEFAULT_CHAT_FRAME:AddMessage("Hardcore Death Logged (" .. HCDeath.tablelength() .. " Deaths)", info.r, info.g, info.b, info.id)
-	HCDeath.resetVariables()
+	DEFAULT_CHAT_FRAME:AddMessage("|cfffc5100Hardcore Death Logged (" .. HCDeath.tablelength() .. " Deaths)|r")
 	logged = true
 end
 
@@ -61,8 +57,7 @@ function HCDeath.friendSlots()
 	local numFriends = GetNumFriends()
 	if numFriends > 48 then
 		local requiredSlots = 50 - numFriends
-		local info = ChatTypeInfo["SYSTEM"]
-		DEFAULT_CHAT_FRAME:AddMessage("Unable to log hardcore death due to friend list limit, please remove " .. requiredSlots .. " friend(s) to enable logging.", info.r, info.g, info.b, info.id)
+		DEFAULT_CHAT_FRAME:AddMessage("|cfffc5100Unable to log hardcore death due to friend list limit, please remove " .. requiredSlots .. " friend(s) to enable logging|r")
 		return false
 	else
 		return true
@@ -85,9 +80,9 @@ HCDeath:SetScript("OnEvent", function()
 	_, _, alreadyfriend = string.find(arg1,"(%a+) is already your friend")
 	_, _, removedfriend = string.find(arg1,"(%a+) removed from friends")
 
-	if (hcdeath) then
+	if (hcdeath and (not playerName)) then
 		local slots = HCDeath.friendSlots()
-		if (slots) then
+		if slots then
 			-- table.insert(HCDeaths, date("!%y%m%d%H%M") .. "," .. arg1) -- log default message
 			playerName = hcdeath
 
@@ -97,28 +92,34 @@ HCDeath:SetScript("OnEvent", function()
 			_, _, pvp = string.find(arg1,"(PvP)")
 			_, _, environment = string.find(arg1,"(natural causes)")
 
-			if environment then
-				deathType = "PVE"
-			elseif pvp then 
+			if pvp then 
 				deathType = "PVP"
 				_, _, killerName = string.find(arg1,"to%s+(%a+)%s+at")
 			else
 				deathType = "PVE"
-				_, _, killerName = string.find(arg1,"to%s+(.-)%s*%(")
-				_, _, killerLevel = string.find(arg1,"%(level%s*(.-)%).-at")			
+				if not environment then
+					_, _, killerName = string.find(arg1,"to%s+(.-)%s*%(")
+					_, _, killerLevel = string.find(arg1,"%(level%s*(.-)%).-at")
+				end
 			end
 
-			-- disable system messages			
-			ChatFrame_RemoveMessageGroup(ChatFrame1, "SYSTEM")
-			
-			if (deathType == "PVE") then
-				AddFriend(playerName)
-			elseif (deathType == "PVP") then
-				AddFriend(playerName)
-				AddFriend(killerName)
-			end			
-		end
-		return
+			if environment then
+				killerName = "Natural Causes"
+			else
+				-- disable system messages			
+				ChatFrame_RemoveMessageGroup(ChatFrame1, "SYSTEM")
+				
+				if (deathType == "PVE") then
+					killerClass = "NPC"
+					AddFriend(playerName)
+				elseif (deathType == "PVP") then
+					AddFriend(playerName)
+					AddFriend(killerName)
+				end
+			end
+
+			return
+		end	
 	end
 
 	if (playerName) then
@@ -134,7 +135,7 @@ HCDeath:SetScript("OnEvent", function()
 							zone = area
 							
 							if addedfriend then
-								RemoveFriend(playerName)								
+								RemoveFriend(playerName)
 							end
 						end
 					end
@@ -152,29 +153,41 @@ HCDeath:SetScript("OnEvent", function()
 								killerClass = class
 
 								if addedfriend then
-									RemoveFriend(killerName)									
+									RemoveFriend(killerName)
 								end
 							end
 						end
 					end
 				end
+			end
+		end
+
+		if removedfriend then
+			-- log death
+			if (deathType == "PVE") then
+				if playerClass then
+					HCDeath.logDeath()
+				end
+			elseif (deathType == "PVP") then
+				if (playerClass and killerClass) then
+					HCDeath.logDeath()				
+				end
 			end			
 		end
-
-		-- log death
-		if (deathType == "PVE") then
-			if playerClass then
-				HCDeath.logDeath()
-			end
-		elseif (deathType == "PVP") then
-			if playerClass and killerClass then
-				HCDeath.logDeath()				
+		
+		if logged then
+			-- enable system messages
+			if (deathType == "PVE") then
+				if playerClass then				
+					ChatFrame_AddMessageGroup(ChatFrame1, "SYSTEM")
+					HCDeath.resetVariables()
+				end
+			elseif (deathType == "PVP") then
+				if (playerClass and killerClass) then				
+					ChatFrame_AddMessageGroup(ChatFrame1, "SYSTEM")
+					HCDeath.resetVariables()
+				end
 			end
 		end
-	end
-
-	if (removedfriend and logged) then
-		-- enable system messages
-		ChatFrame_AddMessageGroup(ChatFrame1, "SYSTEM")
 	end
 end)
