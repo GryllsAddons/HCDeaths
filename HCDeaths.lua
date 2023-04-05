4da1d7266c3b402aa5aea44afaf12ee2b5fe1919
GryllsAddons <107083057+GryllsAddons@users.noreply.github.com>
2023-04-05 17:24:37 +0100

    fix system messages

--- HCDeaths.lua
+++ HCDeaths.lua
@@ -19,8 +19,8 @@ local playerClass
 local killerName
 local killerLevel
 local killerClass
-local logged
 
+local enableMessages
 
 function HCDeath.resetVariables()
 	sdate = nil
@@ -46,8 +46,6 @@ end
 function HCDeath.logDeath()
 	table.insert(HCDeaths, sdate .. "," .. stime .. "," .. tostring(deathType) .. "," .. tostring(zone) .. ","  .. tostring(playerName) .. "," .. tostring(playerLevel) .. "," .. tostring(playerClass) .. "," .. tostring(killerName) .. "," .. tostring(killerLevel) .. "," .. tostring(killerClass))
 	DEFAULT_CHAT_FRAME:AddMessage("|cfffc5100Hardcore Death Logged (" .. HCDeath.tablelength() .. " Deaths)|r")
-	HCDeath.resetVariables()
-	logged = true
 end
 
 function HCDeath.friendSlots()
@@ -68,7 +66,7 @@ end
 -- PVE = "A tragedy has occurred. Hardcore character PLAYERNAME has fallen to MOBNAME1 MOBNAME2 (level KILLERLEVEL) at level PLAYERLEVEL. May this sacrifice not be forgotten."
 -- PVE = "A tragedy has occurred. Hardcore character PLAYERNAME died of natural causes at level PLAYERLEVEL. May this sacrifice not be forgotten."
 
-HCDeath:SetScript("OnEvent", function()	
+HCDeath:SetScript("OnEvent", function()
 	local hcdeath	
 	local addedfriend
 	local alreadyfriend
@@ -111,17 +109,17 @@ HCDeath:SetScript("OnEvent", function()
 			AddFriend(playerName)
 			if (deathType == "PVP") then
 				AddFriend(killerName)
-			end
+			end			
 			return
-		end		
+		end
 	end
 
+	-- if hardcore death
 	if (playerName) then
 		-- if the player has been added to friends
 		if (addedfriend or alreadyfriend) then
 			-- disable system messages
-			-- ChatFrame_RemoveMessageGroup(ChatFrame1, "SYSTEM")
-			ChatFrame1:UnregisterEvent("CHAT_MSG_SYSTEM")
+			ChatFrame_RemoveMessageGroup(ChatFrame1, "SYSTEM")
 
 			-- get player info
 			if ((addedfriend == playerName) or (alreadyfriend == playerName)) then
@@ -138,7 +136,7 @@ HCDeath:SetScript("OnEvent", function()
 					end
 					RemoveFriend(playerName)
 				end
-			end			
+			end
 			
 			if (deathType == "PVP") then
 				-- get killer info
@@ -155,7 +153,7 @@ HCDeath:SetScript("OnEvent", function()
 						end
 					end
 					RemoveFriend(killerName)
-				end				
+				end
 			end
 
 			-- log death if possible
@@ -173,16 +171,28 @@ HCDeath:SetScript("OnEvent", function()
 			return
 		end
 
-		-- if the player has been removed from friends
 		if (removedfriend) then
-			-- if death has been logged
-			if (logged) then
-				-- enable system messages				
-				-- ChatFrame_AddMessageGroup(ChatFrame1, "SYSTEM")
-				ChatFrame1:RegisterEvent("CHAT_MSG_SYSTEM")
-				logged = nil
-			end
-			return
+			-- the removed friend message will appear after the return		
+			if (deathType == "PVE") then
+				if (removedfriend == playerName) then
+					HCDeath.resetVariables()
+					enableMessages = true
+					return
+				end
+			elseif (deathType == "PVP") then
+				if (removedfriend == killerName) then					
+					HCDeath.resetVariables()
+					enableMessages = true
+					return
+				end
+			end	
 		end
+	end
+
+	-- catches the removed friend message
+	if (enableMessages) then
+		-- enable system messages
+		ChatFrame_AddMessageGroup(ChatFrame1, "SYSTEM")
+		enableMessages = nil
 	end
 end)
